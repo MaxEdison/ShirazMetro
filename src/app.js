0357d6fe3ba0d6a59f26b5c43f5b4b230cb41c77
Max Edison <amirhossein.heidari.email@gmail.com>
2026-08-26 05:13:55 +0330

    feat: add basic express server with API info endpoint

--- src/app.js
+++ src/app.js
@@ -2,15 +2,21 @@ import express from 'express';
 
 const app = express();
 
+const API_INFO = {
+  message: 'Welcome to the ShirazMetro API!',
+  version: '1.0.0',
+  endpoints: {
+    stations: '/api/v1/stations/stations',
+    schedules: '/api/v1/schedules/calculate?startStation=STATION&destinationStation=STATION&holiday=yes/no&line=line1/line2'
+  }
+};
+
 app.get('/', (req, res) => {
-  res.json({ 
-    message: 'Welcome to the ShirazMetro API!',
-    version: '1.0.0',
-    endpoints:{
-        stations: '/api/v1/stations/stations',
-        schedules: '/api/v1/schedules/calculate?startStation=STATION&destinationStation=STATION&holiday=yes/no&line=line1/line2'
-    }
-   });
+  res.json(API_INFO);
+});
+
+app.get('/api', (req, res) => {
+  res.json(API_INFO);
 });
 
 export default app;
