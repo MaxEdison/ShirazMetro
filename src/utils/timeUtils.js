834051624ecea4032628e23a81ccb927423dafd5
Max Edison <amirhossein.heidari.email@gmail.com>
2026-08-26 05:51:10 +0330

    feat: Add timeToDate function to convert time string to Date object

---
+++ src/utils/timeUtils.js
@@ -0,0 +1,8 @@
+// commit message
+
+export function timeToDate(timeStr) {
+    const [hours, minutes] = timeStr.split(":").map(Number);
+    const date = new Date();
+    date.setHours(hours, minutes, 0, 0);
+    return date;
+}
