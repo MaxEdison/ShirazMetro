4b706ce770c6497fe6499c53130a95d1b0ff3e82
Max Edison <amirhossein.heidari.email@gmail.com>
2026-08-28 11:32:43 +0330

    (app.js) add schedule calculation endpoint

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import routes from './routes/index.js';
 
 const app = express();
 
@@ -18,5 +19,7 @@ app.get('/', (req, res) => {
 app.get('/api', (req, res) => {
   res.json(API_INFO);
 });
+
+app.use('/api/v1', routes);
 
 export default app;
